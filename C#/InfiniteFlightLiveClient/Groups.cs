e6ade6e07fdf57bdaa643c8da85b8b4662c3a496
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2020-10-27 14:02:05 +1100

    Restructure C# Sample

---
+++ C#/InfiniteFlightLiveClient/Groups.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfiniteFlightLiveClient
+{
+    public static class Groups
+    {
+        public static Guid Ifatc = new Guid("DF0F6341-5F6A-40EF-8B73-087A0EC255B5");
+        public static Guid Moderators = new Guid("8C93A113-0C6C-491F-926D-1361E43A5833");
+        public static Guid Staff = new Guid("D07AFAD8-79DF-4363-B1C7-A5A1DDE6E3C8");
+    }
+}
