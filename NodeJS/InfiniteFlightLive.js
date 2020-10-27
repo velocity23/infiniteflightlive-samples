f4440f6a7a1c2d4653ab987c5a83a0c7d43b46e8
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2020-10-27 13:49:50 +1100

    Update InfiniteFlightLive.js
    Use const instead of let for objects

--- NodeJS/InfiniteFlightLive.js
+++ NodeJS/InfiniteFlightLive.js
@@ -7,8 +7,8 @@ const APIKEY = process.env.IfLiveKey;
 const URLBASE = "https://api.infiniteflight.com/public/v2";
 
 exports.sessions = async () => {
-    let result = await axios.get(URLBASE + "/sessions?apikey=" + APIKEY);
-    let response = result.data;
+    const result = await axios.get(URLBASE + "/sessions?apikey=" + APIKEY);
+    const response = result.data;
     if (response.errorCode != 0) {
         await Promise.reject(new Error("Invalid API Response Code. Expected 0, received " + response.errorCode));
     }
@@ -17,8 +17,8 @@ exports.sessions = async () => {
 }
 
 exports.flights = async (sessionId) => {
-    let result = await axios.get(URLBASE + "/flights/" + sessionId + "?apikey=" + APIKEY)
-    let response = result.data;
+    const result = await axios.get(URLBASE + "/flights/" + sessionId + "?apikey=" + APIKEY)
+    const response = result.data;
     if (response.errorCode != 0) {
         await Promise.reject(new Error("Invalid API Response Code. Expected 0, received " + response.errorCode));
     }
@@ -27,8 +27,8 @@ exports.flights = async (sessionId) => {
 }
 
 exports.flightPlans = async (sessionId) => {
-    let result = await axios.get(URLBASE + "/flightplans/" + sessionId + "?apikey=" + APIKEY);
-    let response = result.data;
+    const result = await axios.get(URLBASE + "/flightplans/" + sessionId + "?apikey=" + APIKEY);
+    const response = result.data;
     if (response.errorCode != 0) {
         await Promise.reject(new Error("Invalid API Response Code. Expected 0, received " + response.errorCode));
     }
@@ -37,8 +37,8 @@ exports.flightPlans = async (sessionId) => {
 }
 
 exports.atc = async (sessionId) => {
-    let result = await axios.get(URLBASE + "/atc/" + sessionId + "?apikey=" + APIKEY);
-    let response = result.data;
+    const result = await axios.get(URLBASE + "/atc/" + sessionId + "?apikey=" + APIKEY);
+    const response = result.data;
     if (response.errorCode != 0) {
         await Promise.reject(new Error("Invalid API Response Code. Expected 0, received " + response.errorCode));
     }
@@ -47,8 +47,8 @@ exports.atc = async (sessionId) => {
 }
 
 exports.gradeTable = function (userId) => {
-    let result = await axios.get(URLBASE + "/user/grade/" + userId + "?apikey=" + APIKEY);
-    let response = result.data;
+    const result = await axios.get(URLBASE + "/user/grade/" + userId + "?apikey=" + APIKEY);
+    const response = result.data;
     if (response.errorCode != 0) {
         await Promise.reject(new Error("Invalid API Response Code. Expected 0, received " + response.errorCode));
     }
@@ -57,10 +57,10 @@ exports.gradeTable = function (userId) => {
 }
 
 exports.userStats = async (userIds) => {
-    let result = await axios.post(URLBASE + "/user/stats?apikey=" + APIKEY, {
+    const result = await axios.post(URLBASE + "/user/stats?apikey=" + APIKEY, {
         userIds: userIds
     });
-    let response = result.data;
+    const response = result.data;
     if (response.errorCode != 0) {
         await Promise.reject(new Error("Invalid API Response Code. Expected 0, received " + response.errorCode));
     }
