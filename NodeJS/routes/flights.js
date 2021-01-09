babfd1034dd0613f2097b1149eb9b413d1126d8d
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2021-01-09 13:28:07 +1100

    Add NodeJS Route Endpoint

--- NodeJS/routes/flights.js
+++ NodeJS/routes/flights.js
@@ -61,4 +61,33 @@ router.get('/flightplan', async (req, res) => {
     res.end();
 });
 
+router.get('/route', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.CORS_DOMAIN);
+    res.contentType('application/json');
+
+    let data;
+    if (typeof req.query.flight === 'undefined' || !req.query.flight) {
+        res.status(400);
+        data = JSON.stringify({
+            error: 400,
+            text: 'Bad Request',
+        });
+    } else {
+        try {
+            data = JSON.stringify(
+                await infiniteflight.flightRoute(req.query.flight)
+            );
+        } catch {
+            res.status(500);
+            data = JSON.stringify({
+                error: 500,
+                text: 'Internal Server Error',
+            });
+        }
+    }
+
+    res.write(data);
+    res.end();
+});
+
 module.exports = router;
